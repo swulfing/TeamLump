81217d18bff02f3a0c343f7524c6c690ee12a044
Sophie <sophiewulfing@gmail.com>
2022-07-07 14:37:57 -0400

    Finish up MA_DMF Data Clean for now. Pending questions

--- DatatoClean/DataClean_Script.R
+++ DatatoClean/DataClean_Script.R
@@ -1,14 +1,16 @@
 library(dplyr)
 
-setwd("GitHub/TeamLump/DatatoClean")
+setwd("GitHub/TeamLump")
 
 
 # MA_DMF DATA CLEAN -------------------------------------------------------
-MA_DMF <- data.frame(read.csv("MA_DMF/MA_DMF_combined.csv"))
+MA_DMF <- data.frame(read.csv("DatatoClean/MA_DMF/MA_DMF_combined.csv"))
 
-#Get rid of negative catch - Reduces from 8087 to 65 datapoints
+#Getw rid of negative catch.change 0 weights to NA's- Reduces from 8087 to 65 datapoints
 MA_DMF_clean <- MA_DMF %>%
-  filter(NUM !=0 )
+  filter(NUM !=0 ) %>%
+  mutate(WT = na_if(WT, 0))
+
 #Read BEGIN_EST as Date in R
 MA_DMF_clean$BEGIN_EST <- as.POSIXct(MA_DMF_clean$BEGIN_EST, format = "%d-%b-%y")
 
@@ -21,14 +23,26 @@ MA_DMF_clean <- MA_DMF_clean %>%
          STATION = STAT,
          DEPTH = SETDEPTH,
          TEMP_BOTTOM = BOTTEMP,
-         WEIGHT_AVG = WT) %>%
-  mutate(DATASOURCE = "MA_DMF", .before = LON)
+         NO_TOW = NUM,
+         WEIGHT_TOT = WT) %>%
+  mutate(DATA_SOURCE = "MA_DMF", .before = LON)
 
+#Survey ID's are not unique - Address later?
 MA_DMF_clean %>% count(SURVEY_ID, sort = TRUE)
+  
+#Duplicate columns based on no of lumpfish caught
+MA_DMF_clean <- as.data.frame(lapply(MA_DMF_clean, rep, MA_DMF_clean$NO_TOW))
+
+MA_DMF_clean <- MA_DMF_clean %>%
+  mutate(WEIGHT_AVG = WEIGHT_TOT/NO_TOW)
+
 
 #Bigger questions/to do:
   
   #Check metadata for what number sin STRATUM and STATION refer to
   #Check metadata for NUM/WT explanation
-  #I'm assuming we'll have one row/lumpfish. should I take average wt of each? or have a separate col when wt is averaged?
+  #Survey ID's are not unique so some duplicate rows are from the same tow and others are different. What do?
+      #MA_DMF_clean %>% count(SURVEY_ID, sort = TRUE)
+  #Is this sufficiently clean?
+
 
